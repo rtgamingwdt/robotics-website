48350e442ef43e87c45cac6f46f7a8b86a78e984
RT <rustytrim11@gmail.com>
2024-04-21 17:27:42 -0400

    feat: removed unused links from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,17 +8,16 @@ export default function Navbar({ showBackground }: { showBackground: boolean }) 
     return (
         <div className="navbar" style={{ color: showBackground ? "white" : "black", backgroundColor: showBackground ? "#09090b" : "transparent" }}>
             <div className="left-side">
-                <Image src={""} width={64} height={64} alt="logo" />
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img className="logo" src={"/logo.jpg"} alt="logo" />
                 <h1>Team 7202 - Cyber Orioles</h1>
             </div>
             <div className="right-side">
                 <Link href={"/"}>Home</Link>
                 <DropdownLink href={"/about-us"} display={[
-                    <Link key={0} href={"/about-us#upcoming-events"}>Upcoming Events</Link>
+                    <Link key={0} href={"/about-us/upcoming-events"}>Upcoming Events</Link>
                 ]}>About Us</DropdownLink>
-                <Link href={"/sponsors"}>Sponsors</Link>
-                <Link href={"/past-events"}>Past Events</Link>
-                <DropdownLink href={"/history"} display={history.map((hist) => <Link href={`${hist.year}-${hist.name}`} key={hist.year}>{`${hist.year} - ${hist.name}`}</Link>)}>History</DropdownLink>
+                <DropdownLink href={"/history"} display={history.map((hist) => <Link href={`/history/${hist.year}-${hist.name.replaceAll(' ', "%20")}`} key={hist.year}>{`${hist.year} - ${hist.name}`}</Link>)}>History</DropdownLink>
                 <Link href={"/resources"}>Resources</Link>
             </div>
         </div>
